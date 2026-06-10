fe84fc29b8ae6ef659a78136792c4ab57e7f681d
Entity378 <150670165+Entity378@users.noreply.github.com>
2026-06-10 11:08:09 +0200

    Add null check for dungeonGenerator

--- Patches/RoundManager_Patches.cs
+++ Patches/RoundManager_Patches.cs
@@ -28,6 +28,9 @@ public static class RoundManager_Patches
             Backrooms.Instance = null;
         }
 
+        if (__instance.dungeonGenerator == null || __instance.dungeonGenerator.Root == null)
+            return;
+        
         var dungenRoot = __instance.dungeonGenerator.Root.transform.position;
         var backroomsGo = GameObject.Instantiate(Plugin.Instance.BackroomsPrefab, new Vector3(5000, dungenRoot.y, 0), Quaternion.identity);
         backroomsGo.GetComponent<NetworkObject>().Spawn(true);
