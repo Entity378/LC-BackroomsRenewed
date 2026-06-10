d678bd37b940568a7bc5670991777ca719ce582e
Entity378 <150670165+Entity378@users.noreply.github.com>
2026-06-10 11:50:40 +0200

    Fix dungeon root position handling in RoundManager

--- Patches/RoundManager_Patches.cs
+++ Patches/RoundManager_Patches.cs
@@ -28,10 +28,12 @@ public static class RoundManager_Patches
             Backrooms.Instance = null;
         }
 
-        if (__instance.dungeonGenerator == null || __instance.dungeonGenerator.Root == null)
-            return;
-        
-        var dungenRoot = __instance.dungeonGenerator.Root.transform.position;
+        var dungenRoot = 50;
+        if (__instance.dungeonGenerator != null && __instance.dungeonGenerator.Root != null)
+        {
+            dungenRoot = __instance.dungeonGenerator.Root.transform.position.y;
+        }
+
         var backroomsGo = GameObject.Instantiate(Plugin.Instance.BackroomsPrefab, new Vector3(5000, dungenRoot.y, 0), Quaternion.identity);
         backroomsGo.GetComponent<NetworkObject>().Spawn(true);
         
